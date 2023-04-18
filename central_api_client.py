15a8348bc1ca601d762aec3bae7b1640060f7e2b
Ubuntu <ubuntu@ip-172-31-25-149.us-west-1.compute.internal>
2023-04-18 16:29:00 +0000

    Added central_get_presence function

--- central_api_client.py
+++ central_api_client.py
@@ -5,8 +5,11 @@ from pprint import pprint
 import json
 import logging
 from threading import Timer
-from time import sleep
+from time import sleep, time
 import sys
+import time
+import json
+import pdb
 
 # influxdb client
 from influxdb import InfluxDBClient
@@ -51,16 +54,27 @@ class RepeatedTimer(object):
 
 def central_get_data():
     global central_obj
-    central_get_apprf()
-    # central_get_presence()
+    central_sites_list = central_get_sites()
+    #central_get_apprf(central_sites_list)
+    central_get_presence(central_sites_list)
 
-def central_get_apprf():
+from pycentral.monitoring import Sites
+def central_get_sites():
+    global central_obj
+    s = Sites()
+    site_list = s.get_sites(central_obj)
+    #print(site_list)
+    return [{'name': site['site_name'], 'id': site['site_id']} for site in site_list['msg']['sites']]
+
+def central_get_apprf(site_list):
     global central_obj
     # Sample API call using 'ArubaCentralBase.command()'
     # GET groups from Aruba Central
     apiPath = "/apprf/datapoints/v2/topn_stats"
     apiMethod = "GET"
-    sites = ["ATM-Demo"]
+    
+    sites = [site['name'] for site in site_list]
+    #sites = ["ATM-Demo"]
     sites_data = {}
     for site in sites:
         apiParams = {
@@ -75,40 +89,96 @@ def central_get_apprf():
         sites_data[site]['name'] = site
 
     #pprint(sites_data)
-    update_influxdb(sites_data)
+    update_influxdb('apprf', sites_data)
 
-def update_influxdb(sites_data):
+def central_get_presence(site_list):
+    global central_obj
+    #siteList = [160, 431, 2, 130, 81, 124]
+    presenceObj = {}
+    startTime = int(time.time() - 300)
+    endTime = int(time.time())
+    sites = [site['id'] for site in site_list]
+    for siteID in sites:
+        apiPath = "/presence/v3/insights/sites/aggregates"
+        apiMethod = "GET"
+        apiParams = {
+			"start_time": startTime,
+			"end_time": endTime,
+			"site_id": siteID
+		}
+        base_resp = central_obj.command(apiMethod=apiMethod,
+                                apiPath=apiPath,
+                                apiParams=apiParams)
+        if (base_resp['code'] == 200):
+            presenceData = base_resp['msg']['data'][0]
+            presenceKeys = ['drawrate_count', 'dwelltime_count', 'loyal_visitors', 'new_visitors', 'passerby_count', 'total_connected', 'visitor_count', 'visits']
+            emptySite = True
+            for key in presenceKeys:
+                if presenceData[key] != None:
+                    emptySite = False
+                    continue
+            # pdb.set_trace()
+            if (not emptySite):
+                presenceObj[presenceData['site_name']] = presenceData
+    update_influxdb('presence', presenceObj)
+
+
+def update_influxdb(topic, sites_data):
     global influxdb_obj
     json_body = []
-    for site in sites_data.keys():
-        data = sites_data[site]
-        timestamp = data['result']['app_cat'][0]['timestamp']
-        field_data = {
-            "measurement": "apprfData",
-            "tags": {
-                "topic": "apprf",
-                "site": site
-            },
-            "time": timestamp,
-            "fields": {}
-        }
-        field_tmp = {}
+    if (topic == 'apprf'):
+        for site in sites_data.keys():
+            data = sites_data[site]
+            timestamp = data['result']['app_cat'][0]['timestamp']
+            field_data = {
+                "measurement": "apprfData",
+                "tags": {
+                    "topic": "apprf",
+                    "site": site
+                },
+                "time": time(),
+                "fields": {}
+            }
+            field_tmp = {}
         for app_data in data['result']['app_cat']:
+            field_data['tags']['app_cat'] = app_data['name']
             field_tmp['name'] = app_data['name']
             field_tmp['percent_usage'] = app_data['percent_usage']
-            field_data["fields"] = field_tmp
+            field_data["fields"] = field_tmp.copy()
             json_body.append(field_data.copy())
+            pprint('*'*50)
+            pprint(json_body)
+            #result = influxdb_obj.write_points(points=json_body.copy(), database='atm23')
+            #print(f'AppRF Database write: {result}')
     
+    elif (topic == 'presence'):
+        for site_data in sites_data.values():
+            site_name = site_data['site_name']
+            site_data.pop('site_name')
+            site_data.pop('store_name')
+            #timestamp = data['result']['app_cat'][0]['timestamp']
+            field_data = {
+                "measurement": "presenceRESTData",
+                "tags": {
+                    "topic": "presence",
+                    "site": site_name
+                },
+                "time": int(time.time()),
+                "fields": site_data
+            }
+            json_body.append(field_data.copy())
+
     try:
-        result = influxdb_obj.write_points(points=json_body, database='atm23')
+        result = influxdb_obj.write_points(points=json_body.copy(), database='atm23')
         #print(streaming_data['topic'] + " Database write: "+ result)
-        print(f'AppRF Database write: {result}')
+        print(f'{topic} Database write: {result}')
         if result == False:
             print("DB push failed!!!")
     except Exception as err:
         print(err)
 
 def establish_influx_conn(host='', port=8086, username='', password='', ssl=True, verify_ssl=True, database=''):
+    #import pdb
     global influxdb_obj
     host = 'localhost'
     port = 8086
@@ -118,7 +188,7 @@ def establish_influx_conn(host='', port=8086, username='', password='', ssl=True
     verify_ssl = False
     client = None
     database = 'atm23'
-
+    #pdb.set_trace()
     try:
         # Create connection
         influxdb_obj = InfluxDBClient(host, port, username, password, ssl, verify_ssl)
@@ -166,7 +236,8 @@ if __name__ == "__main__":
     ssl_verify = True
     central_obj = ArubaCentralBase(central_info=central_info,
                             ssl_verify=ssl_verify)
-
+    
+    
     # replace prom_collector with function we want repeated
     rt = RepeatedTimer(REPEAT_NSEC, central_get_data) # it auto-starts, no need of rt.start()
 
